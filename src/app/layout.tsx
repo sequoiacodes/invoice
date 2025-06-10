8115c5179b5e3eb8516e5d47bbc6cb3d769dfb56
aalokkunwar <aalokjungkunwar1@gmail.com>
2025-06-10 20:00:52 +0545

    tt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,23 +18,23 @@ export const metadata = {
     icon: "/favicon.ico",
   },
   openGraph: {
-    title: "App Maker",
+    title: "Bill Maker",
     description: "A short description of your application.",
     url: "https://bill.sequoiacodes.com.np",
-    siteName: "App Maker",
+    siteName: "Bill Maker",
     images: [
       {
         url: "https://bill.sequoiacodes.com.np/favicon.ico",
         width: 1200,
         height: 630,
-        alt: "App Maker",
+        alt: "Bill Maker",
       },
     ],
     type: "website",
   },
   twitter: {
     card: "summary_large_image",
-    title: "App Maker",
+    title: "Bill Maker",
     description: "A short description of your application.",
     images: ["https://bill.sequoiacodes.com.np/twitter-image.jpg"],
     creator: "@yourhandle",
@@ -51,16 +51,16 @@ export default function RootLayout({ children }: RootLayoutProps) {
         <meta name="keywords" content="Next.js, SEO, ThemeProvider, Web App" />
         <meta name="author" content="Your Name" />
         <link rel="icon" href="/favicon.ico" />
-        <meta property="og:title" content="App Maker" />
+        <meta property="og:title" content="Bill Maker" />
         <meta property="og:description" content="A short description of your application." />
         <meta property="og:image" content="https://bill.sequoiacodes.com.np/og-image.jpg" />
         <meta property="og:url" content="https://bill.sequoiacodes.com.np" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="App Maker" />
+        <meta name="twitter:title" content="Bill Maker" />
         <meta name="twitter:description" content="A short description of your application." />
         <meta name="twitter:image" content="https://bill.sequoiacodes.com.np/twitter-image.jpg" />
         <meta name="twitter:creator" content="@yourhandle" />
-        <title>App Maker</title>
+        <title>Bill Maker</title>
       </head>
       <body>
         <ThemeProvider
